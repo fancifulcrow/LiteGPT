5b5855e54cc11230296050273194e15cf80f7b32
Ngozi <124025329+fancifulcrow@users.noreply.github.com>
2024-09-18 23:51:04 +0100

    refactor plotting utils

--- modules/utils.py
+++ modules/utils.py
@@ -1,8 +1,16 @@
+import matplotlib.pyplot as plt
 
 
 def count_parameters(model) -> int:
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 
+def loss_curve(losses:list[float], title:str) -> None:
+    plt.figure(figsize=(16, 9))
+    plt.plot(losses)
+    plt.title(f"{title.title()}")
+    plt.xlabel("Step")
+    plt.ylabel("Loss")
+    plt.grid(True)
     
     plt.savefig("loss_curve.png")
